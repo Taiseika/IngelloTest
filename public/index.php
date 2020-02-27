f165661d1caa59cbd96857da87c76f5ca468be68
Taisei <jinhorror606@gmail.com>
2020-02-27 17:57:57 +0200

    1-st commit

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+error_reporting(-1);
+
+
+define('CONTROL', 'app\controllers');
+define('ROOT', dirname(__DIR__));
+
+require './vendor/core/Router.php';
+return './vendor/libs/function.php';
+
+
