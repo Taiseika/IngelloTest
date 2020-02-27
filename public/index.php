b1fd42683890c8970a4905a97266feb4cd1b79b8
Taisei <jinhorror606@gmail.com>
2020-02-27 19:01:27 +0200

    6-st commit

--- public/index.php
+++ public/index.php
@@ -5,18 +5,23 @@ $query = rtrim($_SERVER['QUERY_STRING'], '/');
 error_reporting(-1);
 
 define('CONTROL', 'app\controllers\main');
+define('CORE', dirname(__DIR__), 'vendor/core');
 define('ROOT', dirname(__DIR__));
+define('APP', dirname(__DIR__).'/app');
 
 require '../vendor/core/Router.php';
 require '../vendor/libs/function.php';
-require '../app/controllers/main/Site.php';
 
-spl_autoload_register(function (){
-    $file = '';
+spl_autoload_register(function ($class){
+    $file = APP."/controllers/main/$class.php";
+    if (is_file($file)) {
+        require_once $file;
+    }
 });
 
 
 
+//Default routs
 Router::add('^$', ['controller' => 'site', 'action' => 'index']);
 Router::add('(?P<controller>[a-z-)+)/(?P<action>(a-z)+)');
 
